60c43dc347882af6cc688d8176f21c7b2cb41822
dadwalakshay <akshayd00@outlook.com>
2022-10-24 15:41:42 +0530

    setup.zsh updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+
 # mes-configurations
 A set of personalised configurations and dotfiles for UNIX-like machines.
+
+Steps to use:
+- `apt install git vim zsh`
+- Install oh-my-zsh `sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+- `git clone https://github.com/dadwalakshay/mes-configurations.git`
+- `./setup.sh`
+- Voila! Everything is configured.
---
+++ setup.zsh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+echo "Initializing configuration..."
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "Please install oh-my-zsh, refer to README.md"
+	return
+fi
+
+CUR_DIR=$(pwd)
+ZSH_CUSTOM_DIR=${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}
+
+ln -sf $CUR_DIR/zsh/.zshrc $HOME/.zshrc
+
+if [ ! -d "$ZSH_CUSTOM_DIR/themes/spaceship-prompt" ] ; then
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git $ZSH_CUSTOM_DIR/themes/spaceship-prompt
+else
+    cd "$ZSH_CUSTOM_DIR/themes/"
+    git pull
+fi
+
+ln -sf "$ZSH_CUSTOM_DIR/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM_DIR/themes/spaceship.zsh-theme"
+ln -sf $CUR_DIR/zsh/spaceship.zsh $HOME/.config/spaceship.zsh
+
+ln -sf $CUR_DIR/zsh/custom/plugins/shortgit $ZSH_CUSTOM_DIR/plugins/shortgit
+
+if [ ! -d "$ZSH_CUSTOM_DIR/plugins/zsh-autosuggestions" ] ; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM_DIR/plugins/zsh-autosuggestions
+else
+    cd "$ZSH_CUSTOM_DIR/plugins/"
+    git pull
+fi
+
+source $HOME/.zshrc
+
+echo "Configuration completed."
